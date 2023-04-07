3f13a9a15d79704779abd5a162f0cf9ddd01ac46
NguyenHoangHieu1 <NguyenHoangHieu1@users.noreply.github.com>
2023-04-07 04:15:40 +0000

    deploy: 45b044dc408668c0ae01c88a00445294d0ec2d63

---
+++ assets/Container-ddf6d4be.js
@@ -0,0 +1,1 @@
+import{r as t,a as e,j as n}from"./index-0685c264.js";const o=a=>(t.useEffect(()=>{e.init({duration:2e3,once:!0})},[]),n.jsx("section",{"data-aos":"fade-up",className:"container m-auto max-w-6xl",children:a.children})),s=o;export{s as C};
